445b861005de58cd686577d3250be9c9e9095eee
Kael <kael.carvalho@icen.ufpa.br>
2024-02-26 16:52:33 -0300

    Lógica do saque adicionada

--- conta.py
+++ conta.py
@@ -1,13 +1,6 @@
 saldo = 0
-
 while True:
-    print("\n1. Depósito")
-    print("2. Saque")
-    print("3. Extrato")
-    print("4. Sair")
-
-    opcao = input("Escolha uma opção: ")
-
+    opcao = input("\n1. Depósito. \n2. Saque. \n3. Extrato. \n4. Sair\nEscolha uma opção: ")
     if opcao == '1':
         valor_deposito = float(input("Digite o valor do depósito: "))
         if valor_deposito > 0:
@@ -15,20 +8,17 @@ while True:
             print(f'Depósito de R${valor_deposito:.2f} realizado com sucesso.')
         else:
             print('O valor do depósito deve ser maior que zero.')
-
     elif opcao == '2':
         valor_saque = float(input("Digite o valor do saque: "))
-        # -----------------------------------------
-        # Desenvolva a lógica da validação do saldo
-        # -----------------------------------------
-
+        if valor_saque <= saldo:
+            print(f"Saque no valor de R${valor_saque:.2f} foi realizado com sucesso.")
+        else:
+            print("Erro!! Saldo insuficeiente!!")
     elif opcao == '3':
         print(f'\nExtrato bancário:')
         print(f'Saldo atual: R${saldo:.2f}')
-
     elif opcao == '4':
         print("Saindo...")
         break
-
     else:
         print("Opção inválida. Por favor, escolha uma opção válida.")
